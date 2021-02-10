eaea18b390d88b7ce2ce75be3251a7c57670ef24
Chi Leung <chiguan.leung@gmail.com>
2021-02-10 15:56:23 +0000

    Refactor

--- pluralsight/iterators_and_iterables/perfect_binary_tree.py
+++ pluralsight/iterators_and_iterables/perfect_binary_tree.py
@@ -1,7 +1,7 @@
 from itertools import *
-from pluralsight.iterators.depth_first_pre_order import PreOrderIterator
-from pluralsight.iterators.iterator_utils import _is_perfect_length
-from pluralsight.iterators.skip_missing import SkipMissingIterator
+from pluralsight.iterators_and_iterables.depth_first_pre_order import PreOrderIterator
+from pluralsight.iterators_and_iterables.iterator_utils import _is_perfect_length
+from pluralsight.iterators_and_iterables.skip_missing import SkipMissingIterator
 
 
 class PerfectBinaryTree:
