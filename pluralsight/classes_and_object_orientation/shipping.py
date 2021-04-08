d441a62ac8def2e9207e22f044de159dddbc6ced
Chi Leung <chiguan.leung@gmail.com>
2021-04-08 12:53:37 +0100

    Static and class methods with inheritance

--- pluralsight/classes_and_object_orientation/shipping.py
+++ pluralsight/classes_and_object_orientation/shipping.py
@@ -1,3 +1,6 @@
+import iso6346
+
+
 class ShippingContainer:
 
     # Class attributes (shared between all instances of class)
@@ -37,25 +40,37 @@ class ShippingContainer:
         return result
     '''
 
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6)
+        )
+
     # Using factory function to implement method which creates an empty shipping container ("Named constructor")
     # This technique allows us to support multiple constructors with different behaviours
+    # Use extended argument syntax as factory methods will not know about the signature of __init__ in subclasses
+    # In general, good OOP design requires that base classes have no knowledge of subclasses
     @classmethod
-    def create_empty(cls, owner_code):
+    def create_empty(cls, owner_code, **kwargs):
 
-        return cls(owner_code, contents=[])
+        return cls(owner_code, contents=[], **kwargs)
 
     # Another named constructor for placing an iterable series of items in the container
     @classmethod
-    def create_with_items(cls, owner_code, items):
+    def create_with_items(cls, owner_code, items, **kwargs):
 
-        return cls(owner_code, contents=list(items))
+        return cls(owner_code, contents=list(items), **kwargs)
 
-    def __init__(self, owner_code, contents):
+    def __init__(self, owner_code, contents, **kwargs):
 
         # Instance attributes
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = self._make_bic_code(  # Use polymorphic dispatch static method through the instance, self
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
 
 
 # Instance attributes
@@ -77,3 +92,53 @@ c4 = ShippingContainer.create_empty("XYZ")
 print(c4.contents)
 c5 = ShippingContainer.create_with_items("CHI", {"food", "textiles", "minerals"})
 print(c5.contents)
+
+# Checking BIC code
+c6 = ShippingContainer.create_empty("ELF")
+print(c6.bic)
+
+
+# Subclass of ShippingContainer (base class)
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    MAX_CELSIUS = 4.0
+
+    # Override initializer from base class
+    # Use * to make celsius a keyword-only argument
+    def __init__(self, owner_code, contents, *, celsius, **kwargs):
+        # In other OOP languages, constructors at every level in an inheritance hierarchy are called automatically
+        # But in Python we need to explicitly call the base class initializer when overriding it in a derived class
+        super().__init__(owner_code, contents, **kwargs)
+        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot!")
+        self.celsius = celsius
+
+    # Static method with inheritance
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6),
+            category="R"  # Only difference is that we are passing R as category
+        )
+
+
+# Check BIC code
+r1 = RefrigeratedShippingContainer("RCO", ["fish"], celsius=1.0)
+print(r1.bic)
+
+# Class methods are also inherited
+# Object gets created with the appropriate subtype
+# Behaviour of class methods behaving polymorphically is a distinguishing feature of Python
+# These invocations work because the base class __init__ method is inherited into the subclass
+r2 = RefrigeratedShippingContainer.create_empty("XML", celsius=3.0)
+print(r2)
+r3 = RefrigeratedShippingContainer.create_with_items("GUO", ["ice", "peas"], celsius=0.0)
+print(r3)
+
+# Construct instance derived class using factory function in base class
+r4 = RefrigeratedShippingContainer.create_with_items("ESC", ["onions"], celsius=2.0)
+print(r4)
+print(r4.bic)
+print(r4.contents)
+print(r4.celsius)
