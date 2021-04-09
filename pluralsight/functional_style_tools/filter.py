386dbd16da648a0e0dc1844b9d8cc2761b1acea3
Chi Leung <chiguan.leung@gmail.com>
2021-04-09 15:31:56 +0100

    Restructure project

--- pluralsight/functional_style_tools/filter.py
+++
@@ -1,7 +0,0 @@
-positives = filter(lambda x: x > 0, [1, -5, 0, 6, -2, 8])
-print(positives)  # Lazy evaluation so only filter object is printed
-print(list(positives))  # Force evaluation of results using list()
-
-# Passing None as the first argument filters out input elements which evaluate to False in a boolean context
-trues = filter(None, [0, 1, False, True, [], [1, 2, 3], '', 'hello'])
-print(list(trues))  # Filters out 0, False, [], '' since they are all Falsy
