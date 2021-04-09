2c7daa2012d0101b52aa37818c18f0ad1a8aef87
Chi Leung <chiguan.leung@gmail.com>
2021-04-09 11:34:32 +0100

    Customizing str()

--- pluralsight/classes_and_object_orientation/position.py
+++ pluralsight/classes_and_object_orientation/position.py
@@ -20,12 +20,30 @@ class Position:
     def longitude(self):
         return self._longitude
 
+    @property
+    def latitude_hemisphere(self):
+        return "N" if self.latitude >= 0 else "S"
+
+    @property
+    def longitude_hemisphere(self):
+        return "E" if self.longitude >= 0 else "W"
+
     # Instance method, so accepts self
     # repr() is intended for developers, should nearly always override
     # Good practice to include necessary state and to format it such that it looks like a constructor call
     def __repr__(self):
         # Use typename instead of hard-coding Position so that it will work with inheritance
         return f"{typename(self)}(latitude={self.latitude}, longitude={self.longitude})"
+
+    # Default implementation of __str__ delegates to __repr__
+    # Need to override to customize behaviour
+    # str() is generally intended for consumers of system (users, people, UIs, other systems, etc.)
+    # Should be readable and leave out technical implementation details
+    def __str__(self):
+        return (
+            f"{abs(self.latitude)}° {self.latitude_hemisphere}, "
+            f"{abs(self.longitude)}° {self.longitude_hemisphere}"
+        )
 
 
 class EarthPosition(Position):
@@ -56,3 +74,8 @@ mauna_kea = EarthPosition(19.82, -155.47)
 print(mauna_kea)
 olympus_mons = MarsPosition(18.65, -133.8)
 print(olympus_mons)
+
+# Testing out str()
+mount_erebus = EarthPosition(-77.5, 167.2)
+print(str(mount_erebus))
+print("Mount Erebus is located at",  mount_erebus)
