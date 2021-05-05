24e8a1e1159334c039761cbcc91ee1a2079f3082
Chi Leung <chiguan.leung@gmail.com>
2021-05-05 19:23:02 +0100

    Minor changes to comments

--- introspection/objects.py
+++ introspection/objects.py
@@ -1,7 +1,7 @@
 a = 42
 
 # dir() returns a list of attribute names for an instance
-# Both attributes and method names are returned (since methods are essentially attributes which are just callable)
+# Both attributes and method names are returned (since methods are just attributes which are callable)
 print(dir(a))
 
 # Retrieve attribute corresponding to attribute name (equivalent to accessing attribute directly)
@@ -18,7 +18,7 @@ print(a.conjugate.__class__.__name__)
 # Check if object has an attribute of a given name
 print(hasattr(a, 'bit_length'))
 
-# Generally should prefer the more Pythonic "EAFP" style programming of using exception handlers
-# As opposed to the "LBLY" (Look Before You Leap) style of using type tests and attribute existence tests
+# Generally should prefer the more Pythonic EAFP style programming of using exception handlers
+# As opposed to the LBLY (Look Before You Leap) style of using type tests and attribute existence tests
 # Programs using hasattr() can quickly become messy
-# The optimistic approach (try/except) can actually be faster than hasattr(), which uses try/except internally anyway
+# The optimistic approach (try/except) can also be faster than hasattr(), which uses try/except internally anyway
